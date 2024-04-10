8c97574683ed4bd1f0b9ec555c56af9a32bf7c11
Vladimir <vladidimirmvjob@gmail.com>
2024-04-10 13:14:52 +0300

    Исправлен vite config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/vue-sneakers/',
   plugins: [
     vue(),
     vueJsx(),
